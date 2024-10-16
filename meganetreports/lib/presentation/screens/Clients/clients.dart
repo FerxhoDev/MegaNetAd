cdb9eb615ac27806c4eff972124a3b344b667908
Ferdy R Cardona <ferdycard0na11@gmail.com>
2024-10-16 12:30:40 -0600

    Clients future

--- meganetreports/lib/presentation/screens/Clients/clients.dart
+++ meganetreports/lib/presentation/screens/Clients/clients.dart
@@ -1,32 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
 class Cliente {
+  final String id;
   final String nombre;
   final String plan;
   final bool alDia; // true si está al día, false si no lo está
 
-  Cliente(this.nombre, this.plan, this.alDia);
+  Cliente(this.id, this.nombre, this.plan, this.alDia);
 }
 
-class Clients extends StatelessWidget {
+
+class Clients extends StatefulWidget {
   const Clients({super.key});
+  
+
+  @override
+  _ClientsState createState() => _ClientsState();
+}
+
+class _ClientsState extends State<Clients> {
+
+  
+  List<Cliente> clientes = [];
+  List<Cliente> filteredClientes = []; // Lista para filtrar los clientes
+  String searchQuery = ''; // Consulta de búsqueda
+
+  @override
+  void initState() {
+    super.initState();
+    _getClientes(); // Llama la función para obtener los clientes
+  }
+
+  Future<void> _getClientes() async {
+    List<Cliente> fetchedClientes = [];
+
+    try {
+      // Obtener todos los clientes
+      QuerySnapshot clientesSnapshot = await FirebaseFirestore.instance.collection('clientes').get();
+
+      // Obtener el mes actual
+      DateTime now = DateTime.now();
+      int currentMonth = now.month;
+
+      for (var doc in clientesSnapshot.docs) {
+        String idCliente = doc.id;
+        String nombre = doc['nombre'];
+        String plan = doc['plan_nombre'];
+
+        // Verificar los pagos de la subcolección Pagos para este cliente
+        QuerySnapshot pagosSnapshot = await FirebaseFirestore.instance
+            .collection('clientes')
+            .doc(idCliente)
+            .collection('Pagos')
+            .where('mespago', isEqualTo: currentMonth.toString()) // Filtrar por mes actual
+            .get();
+
+        // Determinar si está al día
+        bool alDia = pagosSnapshot.docs.isNotEmpty;
+
+        // Crear la instancia de Cliente
+        fetchedClientes.add(Cliente(idCliente, nombre, plan, alDia));
+      }
+    } catch (e) {
+      print('Error al obtener clientes: $e');
+    }
+
+    setState(() {
+      clientes = fetchedClientes;
+      filteredClientes = fetchedClientes; // Inicialmente muestra todos los clientes
+    });
+  }
+
+  void _filterClientes(String query) {
+    final filtered = clientes.where((cliente) {
+      return cliente.nombre.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+
+    setState(() {
+      searchQuery = query;
+      filteredClientes = filtered;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Lista de clientes de ejemplo
-    final List<Cliente> clientes = [
-      Cliente('Juan Pérez', 'Plan Básico', true),
-      Cliente('Ana García', 'Plan Fast', false),
-      Cliente('Carlos Gómez', 'Plan Super Fast', true),
-      Cliente('María López', 'Plan Básico', false),
-      Cliente('Pedro Rodríguez', 'Plan Fast', true),
-      Cliente('Anahí Miranda', 'plan Plus 225', false),
-      Cliente('Edgar Paz', 'plan Plus 250', true),
-      Cliente('Sofía Mendoza', 'Plan Básico', false),
-    ];
-
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 37, 37, 37),
       appBar: AppBar(
@@ -48,30 +109,23 @@ class Clients extends StatelessWidget {
               color: const Color.fromARGB(255, 50, 50, 50),
               borderRadius: BorderRadius.circular(10),
             ),
-            child: const TextField(
-              style: TextStyle(color: Colors.white70),
-              decoration: InputDecoration(
-                hintText: 'Buscar Cliente',
-                hintStyle: TextStyle(color: Colors.white70),
-                border: InputBorder.none,
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.white70,
-                ),
-              ),
+            child: CupertinoSearchTextField(
+              backgroundColor: Colors.transparent,
+              placeholder: 'Buscar cliente',
+              style: const TextStyle(color: Colors.white70),
+              onChanged: _filterClientes, // Filtra clientes cuando cambia el texto
             ),
           ),
           SizedBox(height: 20.h),
           Expanded(
             // Lista de clientes con ListView.builder
             child: ListView.builder(
-              itemCount: clientes.length,
+              itemCount: filteredClientes.length,
               itemBuilder: (context, index) {
-                final cliente = clientes[index];
+                final cliente = filteredClientes[index];
                 return Card(
                   color: const Color.fromARGB(255, 50, 50, 50),
-                  margin:
-                      EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+                  margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
                   child: ListTile(
                     title: Text(
                       cliente.nombre,
@@ -110,15 +164,4 @@ class Clients extends StatelessWidget {
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: Clients(),
-    builder: (context, child) {
-      // Inicializa ScreenUtil para permitir adaptabilidad a diferentes pantallas
-      ScreenUtil.init(context, designSize: const Size(375, 812));
-      return child!;
-    },
-  ));
 }
