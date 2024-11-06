b9f260b203197c0caecc55b3218da5a243e6924c
Ferdy R Cardona <ferdycard0na11@gmail.com>
2024-11-06 17:16:43 -0600

    Share Tickets Add

--- meganetreports/lib/presentation/screens/ticket/ticket.dart
+++ meganetreports/lib/presentation/screens/ticket/ticket.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 
+import 'dart:io';
+import 'dart:typed_data';
 
 class TicketSend extends StatefulWidget {
   final String nombre;
@@ -32,6 +39,11 @@ class TicketSend extends StatefulWidget {
 }
 
 class _TicketSendState extends State<TicketSend> {
+
+  //Capturar pantalla
+  final controllerT = ScreenshotController();
+
+
   String fechaFormateada = '';
 
   @override
@@ -61,61 +73,68 @@ class _TicketSendState extends State<TicketSend> {
           ),
           IconButton(
             icon: const Icon(Icons.share, color: Colors.white,),
-            onPressed: () {
+            onPressed: () async {
               // Implementar la funcionalidad de compartir aquí
+              final image = await controllerT.capture();
+              if (image == null) return;
+
+              await saveAndShare(image);
             },
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Card(
-            elevation: 8,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Center(
-                    child: Text(
-                      'Comprobante de Pago',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  _buildInfoRow('Cliente', widget.nombre),
-                  _buildInfoRow('Teléfono', widget.telefono),
-                  _buildInfoRow('Plan', widget.plan),
-                  _buildInfoRow('Precio', 'Q${widget.precio}'),
-                  _buildInfoRow('Mes de Pago', widget.mesPago),
-                  _buildInfoRow('Descuento', widget.descuento != null ? '-Q${widget.descuento}' : 'Q0.00'),
-                  const Divider(thickness: 2),
-                  _buildInfoRow('Total', 'Q${widget.total}', isTotal: true),
-                  if (widget.descripcion != null)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16.0),
+      body: Screenshot(
+        controller: controllerT,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Center(
                       child: Text(
-                        'Descripción: ${widget.descripcion}',
-                        style: const TextStyle(fontStyle: FontStyle.italic),
+                        'Comprobante de Pago',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                       ),
                     ),
-                  const SizedBox(height: 20),
-                  Text ('Generado en, Caserío Nueva Jerusalem, San Antonio S, S.M. $fechaFormateada', style: const TextStyle(color: Colors.grey),),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      const Text('Mega', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
-                      const Text('Net', style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)),
-                      SizedBox(width: 10.h),
-                      const Icon(Icons.qr_code, size: 35, color: Colors.grey),
-                    ],
-                  ),
-                ],
+                    const SizedBox(height: 20),
+                    _buildInfoRow('Cliente', widget.nombre),
+                    _buildInfoRow('Teléfono', widget.telefono),
+                    _buildInfoRow('Plan', widget.plan),
+                    _buildInfoRow('Precio', 'Q${widget.precio}'),
+                    _buildInfoRow('Mes de Pago', widget.mesPago),
+                    _buildInfoRow('Descuento', widget.descuento != null ? '-Q${widget.descuento}' : 'Q0.00'),
+                    const Divider(thickness: 2),
+                    _buildInfoRow('Total', 'Q${widget.total}', isTotal: true),
+                    if (widget.descripcion != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(
+                          'Descripción: ${widget.descripcion}',
+                          style: const TextStyle(fontStyle: FontStyle.italic),
+                        ),
+                      ),
+                    const SizedBox(height: 20),
+                    Text ('Generado en, Caserío Nueva Jerusalem, San Antonio S, S.M. $fechaFormateada', style: const TextStyle(color: Colors.grey),),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Text('Mega', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+                        const Text('Net', style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)),
+                        SizedBox(width: 10.h),
+                        const Icon(Icons.qr_code, size: 35, color: Colors.grey),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -161,4 +180,26 @@ ${widget.descripcion != null ? '\nDescripción: ${widget.descripcion}' : ''}
       const SnackBar(content: Text('Detalles del ticket copiados al portapapeles')),
     );
   }
+}
+
+Future<String> saveImage(Uint8List bytes) async {
+  await [Permission.storage].request();
+
+  final time = DateTime.now()
+      .toIso8601String()
+      .replaceAll('.', '_')
+      .replaceAll(':', '_');
+
+  final name = 'Screenshot_$time';
+  final result =
+      await ImageGallerySaver.saveImage(Uint8List.fromList(bytes), name: name);
+
+  return result['filePath'];
+}
+
+Future saveAndShare(Uint8List bytes) async {
+  final directory = await getTemporaryDirectory();
+  final image = File('${directory.path}/image.png');
+  image.writeAsBytesSync(bytes);
+  await Share.shareFiles([image.path], text: 'Le compartimos su comprobante de pago de su servicio de Internet. *MegaNet*');
 }
